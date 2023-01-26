fddaa9a32241c5c4447ebf720ef4cfa160ef2293
Primemerlinian <mwatson0518@gmail.com>
2023-01-26 16:57:11 -0500

    stub up burger nav

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+// Components & Assets
+import Logo from '../../assets/react-logo.png'
+import { NavLink } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <nav className="navigation-bar">
+    <NavLink id='logo' to='/'>
+      <img src={Logo} alt='react-logo'/>
+    </NavLink>
+    <NavLink to='/burgers'>BURGER SHOP</NavLink>
+    </nav>
+  )
+}
+
+export default Nav
